688e9b8585ecae1d5052f5701f4eab017ad4c744
Erica Cannedy <encannedy@gmail.com>
2020-05-31 23:35:54 -0400

    issues with post and delete attempted fix

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,68 +1,76 @@
+const express = require('express');
+const app = express();
 const fs = require('fs');
-
+const path = require ("path");
 let db = require("../db/db");
 
 module.exports = (app) => {
+//initialize notesData
 
-
-let id= "";
 
 //write out your routes to correspond with the front end routes
 //fs readfile and writefile will be in here
 
 //retrieve notes data already in the json object
+let id = "";
 
-app.get("/api/notes",  (req, res)=> {
-    fs.readFile('db/db.json', "utf8",  (error, data) =>{
-        res.json(JSON.parse(data));
-    })
-});
+    // Displays all notes
+    app.get("/api/notes", function (req, res) {
+        fs.readFile('db/db.json',"utf8", function(error, data){
+            res.json (JSON.parse(data))
+        })
+
+    });
 
 //post/write new data to the json object
 //must use json stringify and parse in order to successfully and correctly parse the data
 
-app.post("/api/notes"), async (req,res) =>{
+app.post("/api/notes"), async function(req,res) {
 
-let newNote = req.body;
-
-if (db ==="") {newNote.id = 1 
-} else {
-    newNote.id =db[db.length -1].id +1;
+let notesData= req.body;
+if (db === "") {
+    notesData.id = 1
+} else{
+    notesData.id= db[db.length - 1] + 1;
 }
-   id= newNote.id;
-   
-   db.push(newNote);
-   
-   let newDB = JSON.stringify(db); 
-   
-   await fs.writeFile("db/db.json", newDB, (err) => {
 
+id=notesData.id;
+
+db.push(notesData);
+
+let newDB= JSON.stringify(db);
+
+await fs.writeFile('db/db.json', newDB, (err) =>{
     if (err) throw err;
-    console.log("note saved");
-    res.json(newNote);
+    console.log('note saved');
+    res.json(notesData);
 });
 
-fs.readFile('db/db.json',"utf8", (error, data) =>{
-    db = JSON.parse(data);
-});
+console.log(notesData);
+fs.readFile('db/db.json', "utf8", function(error, data){
+    db=JSON.parse(data);
+})
 
+};
+
+
+//delete old data from the json object/ delete note using id filter
 
 app.delete("/api/notes/:id", function (req, res) {
-    let choiceId = req.params.id;
+    let chosenID = req.params.id;
 
-    let dbUpdate = db.filter(x => {
-        return x.id != choiceId;
+    let updatedDb = db.filter(x => {
+        return x.id != chosenID;
     })
-   console.log(dbUpdate);
+   console.log(updatedDb);
 
-    fs.writeFile("db/db.json", JSON.stringify(dbUpdate), (err) => {
+    fs.writeFile("db/db.json", JSON.stringify(updatedDb), (err) => {
         if (err) throw err;
         console.log('The note has been deleted!');
-        db = dbUpdate;
+        db = updatedDb;
         res.json({ok:true});
     });
 
 });
 
-};
 };
