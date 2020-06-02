ce1cc73d2f52037edf1f8751760fcffac2ef19a8
Erica Cannedy <encannedy@gmail.com>
2020-06-02 18:17:19 -0400

    updates to post function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,76 +1,70 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs');
-const path = require ("path");
-let db = require("../db/db");
+const fs = require("fs");
+const path = require("path");
+//let db = require("../db/db");
 
 module.exports = (app) => {
-//initialize notesData
+  //initialize notesData
 
+  //write out your routes to correspond with the front end routes
+  //fs readfile and writefile will be in here
 
-//write out your routes to correspond with the front end routes
-//fs readfile and writefile will be in here
+  //retrieve notes data already in the json object
+  let id = "";
 
-//retrieve notes data already in the json object
-let id = "";
+  // Displays all notes
+  app.get("/api/notes", function (req, res) {
+    fs.readFile("db/db.json", "utf8", function (error, data) {
+      res.json(JSON.parse(data));
+    });
+  });
 
-    // Displays all notes
-    app.get("/api/notes", function (req, res) {
-        fs.readFile('db/db.json',"utf8", function(error, data){
-            res.json (JSON.parse(data))
-        })
+  //post/write new data to the json object
+  //must use json stringify and parse in order to successfully and correctly parse the data
 
+  app.post("/api/notes", function (req, res) {
+    let notesData = req.body;
+
+    let db = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+
+    if (db.length === 0) {
+      notesData.id = 1;
+    } else {
+      notesData.id = db.length;
+    }
+
+    db.push(notesData);
+
+    let newDB = JSON.stringify(db);
+
+    fs.writeFile("db/db.json", newDB, (err) => {
+      if (err) throw err;
+      console.log("note saved");
+      res.json(notesData);
     });
 
-//post/write new data to the json object
-//must use json stringify and parse in order to successfully and correctly parse the data
+    //   console.log(notesData);
+    //   fs.readFile("db/db.json", "utf8", function (error, data) {
+    //     db = JSON.parse(data);
+    //   });
+  });
 
-app.post("/api/notes"), async function(req,res) {
+  //delete old data from the json object/ delete note using id filter
 
-let notesData= req.body;
-if (db === "") {
-    notesData.id = 1
-} else{
-    notesData.id= db[db.length - 1] + 1;
-}
-
-id=notesData.id;
-
-db.push(notesData);
-
-let newDB= JSON.stringify(db);
-
-await fs.writeFile('db/db.json', newDB, (err) =>{
-    if (err) throw err;
-    console.log('note saved');
-    res.json(notesData);
-});
-
-console.log(notesData);
-fs.readFile('db/db.json', "utf8", function(error, data){
-    db=JSON.parse(data);
-})
-
-};
-
-
-//delete old data from the json object/ delete note using id filter
-
-app.delete("/api/notes/:id", function (req, res) {
+  app.delete("/api/notes/:id", function (req, res) {
     let chosenID = req.params.id;
-
-    let updatedDb = db.filter(x => {
-        return x.id != chosenID;
-    })
-   console.log(updatedDb);
+    let db = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    let updatedDb = db.filter((x) => {
+      return x.id != chosenID;
+    });
+    console.log(updatedDb);
 
     fs.writeFile("db/db.json", JSON.stringify(updatedDb), (err) => {
-        if (err) throw err;
-        console.log('The note has been deleted!');
-        db = updatedDb;
-        res.json({ok:true});
+      if (err) throw err;
+      console.log("The note has been deleted!");
+      db = updatedDb;
+      res.json({ ok: true });
     });
-
-});
-
+  });
 };
